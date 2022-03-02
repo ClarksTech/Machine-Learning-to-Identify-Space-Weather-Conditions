25ae6f8de50d815549b6f8b92c2e7952ac357479
ClarksTech <c.rutter50@gmail.com>
2022-03-02 10:15:56 +0000

    Comments Added

--- COSMIC_2_DATA_READ_IN.py
+++ COSMIC_2_DATA_READ_IN.py
@@ -1,19 +1,22 @@
-import netCDF4 as nc
-import matplotlib.pyplot as plt
+#include the required libraries
+import netCDF4 as nc                # dataset is in netCDF format
+import matplotlib.pyplot as plt     # for graphing data to validate its presence 
 
+#####################################################################
+#################### Open dataset and access TEC ####################
+#####################################################################
+dataFile = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_032\\podTc2_C2E1.2020.032.00.01.0019.G28.02_2019.3430_nc" # path to file
+dataset = nc.Dataset(dataFile)  # Access the dataset using netCDF4 library tools
+print(dataset)                  # printing dataset to display all available fields to verify correct podTEC format
+print(dataset['TEC'])           # print TEC variable to verify the TEC data format
+TEC = dataset['TEC'][:]         # store the entire TEC data in variable TEC
+print(TEC)                      # Print variable TEC to confirm data transfer
+print(dataset['time'])                  # print time variable to verify time format
+measurementTime = dataset['time'][:]    # store entire time data in variable
+print(measurementTime)                  # print variable to confirm data transfer
 
-f = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_032\\podTc2_C2E1.2020.032.00.01.0019.G28.02_2019.3430_nc"
-ds = nc.Dataset(f)
-print(ds)
-print(ds['TEC'])
-TEC = ds['TEC'][:]
-print(TEC)
-
-print(ds['time'])
-measurementTime = ds['time'][:]
-print(measurementTime)
-
-plt.plot(measurementTime, TEC)
-plt.ylabel("TEC along LEO-GPS link (TECU)")
-plt.xlabel("Time of GPS measurement (GPS Second)")
-plt.show()
+####################### Plot TEC against Time #######################
+plt.plot(measurementTime, TEC)                      # plot GPS measurement time on x axis, TEC on y axis
+plt.ylabel("TEC along LEO-GPS link (TECU)")         # label y axis
+plt.xlabel("Time of GPS measurement (GPS Second)")  # label x axis
+plt.show()                                          # display the plot
