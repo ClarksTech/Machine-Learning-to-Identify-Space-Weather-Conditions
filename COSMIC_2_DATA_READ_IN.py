9c31ee4798b542e1d2cc7ea6fc70c1feddde59ad
ClarksTech <c.rutter50@gmail.com>
2022-03-02 10:26:43 +0000

    loop for all files in directory

--- COSMIC_2_DATA_READ_IN.py
+++ COSMIC_2_DATA_READ_IN.py
@@ -1,22 +1,30 @@
 #include the required libraries
 import netCDF4 as nc                # dataset is in netCDF format
 import matplotlib.pyplot as plt     # for graphing data to validate its presence 
+from pathlib import Path            # to iterate over all files in folder
 
 #####################################################################
 #################### Open dataset and access TEC ####################
 #####################################################################
-dataFile = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_032\\podTc2_C2E1.2020.032.00.01.0019.G28.02_2019.3430_nc" # path to file
-dataset = nc.Dataset(dataFile)  # Access the dataset using netCDF4 library tools
-print(dataset)                  # printing dataset to display all available fields to verify correct podTEC format
-print(dataset['TEC'])           # print TEC variable to verify the TEC data format
-TEC = dataset['TEC'][:]         # store the entire TEC data in variable TEC
-print(TEC)                      # Print variable TEC to confirm data transfer
-print(dataset['time'])                  # print time variable to verify time format
-measurementTime = dataset['time'][:]    # store entire time data in variable
-print(measurementTime)                  # print variable to confirm data transfer
+directoryPath = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_032"   # Directory path containing Data
+paths = Path(directoryPath).glob('**/*.3430_nc')                                                                    # Path for all .3430_nc podTEC files
 
-####################### Plot TEC against Time #######################
-plt.plot(measurementTime, TEC)                      # plot GPS measurement time on x axis, TEC on y axis
-plt.ylabel("TEC along LEO-GPS link (TECU)")         # label y axis
-plt.xlabel("Time of GPS measurement (GPS Second)")  # label x axis
-plt.show()                                          # display the plot
+# repeat for all files in directory to show variation
+for path in paths:
+    dataset = nc.Dataset(path)  # Access the dataset using netCDF4 library tools
+    print(dataset)                  # printing dataset to display all available fields to verify correct podTEC format
+    print(dataset['TEC'])           # print TEC variable to verify the TEC data format
+    TEC = dataset['TEC'][:]         # store the entire TEC data in variable TEC
+    print(TEC)                      # Print variable TEC to confirm data transfer
+    print(dataset['time'])                  # print time variable to verify time format
+    measurementTime = dataset['time'][:]    # store entire time data in variable
+    print(measurementTime)                  # print variable to confirm data transfer
+
+    ####################### Plot TEC against Time #######################
+    plt.plot(measurementTime, TEC)                      # plot GPS measurement time on x axis, TEC on y axis
+    plt.ylabel("TEC along LEO-GPS link (TECU)")         # label y axis
+    plt.xlabel("Time of GPS measurement (GPS Second)")  # label x axis
+    plt.show()                                          # display the plot
+
+
+
