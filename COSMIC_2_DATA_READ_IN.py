f8c0eb9cfd2a0c5625b639662d69bb9365cfe035
ClarksTech <c.rutter50@gmail.com>
2022-03-10 14:37:07 +0000

    Converted GPS time to UTC

--- COSMIC_2_DATA_READ_IN.py
+++ COSMIC_2_DATA_READ_IN.py
@@ -1,7 +1,10 @@
-#include the required libraries
-import netCDF4 as nc                # dataset is in netCDF format
-import matplotlib.pyplot as plt     # for graphing data to validate its presence 
-from pathlib import Path            # to iterate over all files in folder
+# include the required libraries
+import netCDF4 as nc                        # dataset is in netCDF format
+import matplotlib.pyplot as plt             # for graphing data to validate its presence 
+from pathlib import Path                    # to iterate over all files in folder
+from datetime import datetime, timedelta
+
+from sympy import timed    # to convert GPS to UTC time
 
 #####################################################################
 #################### Open dataset and access TEC ####################
@@ -20,6 +23,11 @@ for path in paths:
     measurementTime = dataset['time'][:]    # store entire time data in variable
     print(measurementTime)                  # print variable to confirm data transfer
 
+    # convert GPS time to UTC time
+    utcTime = []                    # initialise array to hold utcTime
+    for time in measurementTime:    # convert every measurement
+        utcTime.append(datetime(1980, 1, 6) + timedelta(seconds=time - (37 - 19)))  # GPS and UTS initialised 1980,1,6. Add time delta acounting for leapseconds
+
     # Identify the LEO and PRN satellites used for measurements
     print(dataset.__dict__['leo_id'])           # print LEO ID
     print(dataset.__dict__['prn_id'])           # print PRN ID
@@ -27,11 +35,11 @@ for path in paths:
     prnId = dataset.__dict__['prn_id']          # Store PRN ID
 
     ####################### Plot TEC against Time #######################
-    plt.plot(measurementTime, TEC)                                  # plot GPS measurement time on x axis, TEC on y axis
-    plt.ylabel("TEC along LEO-GPS link (TECU)")                     # label y axis
-    plt.xlabel("Time of GPS measurement (GPS Second)")              # label x axis
-    plt.title(f"TEC plot for PRN ID: {leoId} and LEO ID: {prnId}")  # title with PRN and LEO ID
-    plt.show()                                                      # display the plot
+    plt.plot(utcTime, TEC)                                              # plot GPS measurement time on x axis, TEC on y axis
+    plt.ylabel("TEC along LEO-GPS link (TECU)")                         # label y axis
+    plt.xlabel(f"UTC Time of Measurement on {utcTime[0].year}/{utcTime[0].month}/{utcTime[0].day}") # label x axis
+    plt.title(f"TEC plot for PRN ID: {leoId} and LEO ID: {prnId}")      # title with PRN and LEO ID
+    plt.show()                                                          # display the plot
 
 
 
