265a4c5887f6f6a03bfd09e9e8711a4f7ca254d8
ClarksTech <c.rutter50@gmail.com>
2022-03-29 12:51:53 +0100

    File created for pre-processing of data before ML

---
+++ PreProcess_Cosmic_Data.py
@@ -0,0 +1,3 @@
+# include required libraries
+import numpy as np                          # numpy used for basemap plotting
+import matplotlib.pyplot as plt             # for graphing data to validate its presence 
