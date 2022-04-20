8b50e6cfb5bb628b37dd23636c2254d1121e3db7
ClarksTech <c.rutter50@gmail.com>
2022-04-20 20:25:26 +0100

    Download to start at last file number

--- TEC_Space_Weather_Main.py
+++ TEC_Space_Weather_Main.py
@@ -20,11 +20,12 @@ path = '../FYP_Data'        # path of the directory
 dir = os.listdir(path)      # Getting the list of directories
   
 # Checking if the list is empty or not and download any required files
-if len(dir) == 0:
+if len(dir) < 366:
     print("Directory Empty - Files Need Downloading")
     print("Downloading the Data Files...")
+    start = len(dir) + 32
     # Download whole of 2020 data files
-    for day in range(32, 367, 1):
+    for day in range(start, 367, 1):
         # progress bar
         progress = ((day-32)/(367-32)) * 100
         print("Progress of Data Download: %.2f" %progress, "%",end='\r')
@@ -45,102 +46,112 @@ else:
     print("Directory Populated - No Files Need Downloading")                # if files exist in data folder then no need to download
 
 #####################################################################
-#################### Import the COSMIC 2 Data #######################
+## Generate all CSV files containing Pixel Data for the year 2020 ###
 #####################################################################
-# directory where data is held
-directoryPath = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_032"   # Directory path containing Data
+for day in range(32, 367, 1):
+    # progress bar
+    progress = ((day-32)/(367-32)) * 100
+    print("Progress of CSV Export: %.2f" %progress, "%",end='\r')
 
-# Determine number of files to be imported
-numberOfFiles = Data.getNumFiles(directoryPath)
+    #####################################################################
+    #################### Import the COSMIC 2 Data #######################
+    #####################################################################
+    # directory where data is held
+    directoryPath = "C:\\Users\\crutt\\Documents\\University\\Final Year Project\\FYP_Data\\podTc2_postProc_2020_{day:03d}"   # Directory path containing Data
 
-# Import files to Class list for easy data access
-tecDataList = Data.importDataToClassList(directoryPath, numberOfFiles)
+    # Determine number of files to be imported
+    numberOfFiles = Data.getNumFiles(directoryPath)
 
-#####################################################################
-################# Perform Pre Processing on Data ####################
-#####################################################################
+    # Import files to Class list for easy data access
+    tecDataList = Data.importDataToClassList(directoryPath, numberOfFiles)
 
-# Generate moving averages test
-PrePro.calculateMovingAverages(tecDataList)
+    #####################################################################
+    ################# Perform Pre Processing on Data ####################
+    #####################################################################
 
-# Generate difference between moving average and Tec Diff
-PrePro.calculateDelta(tecDataList)
+    # Generate moving averages test
+    PrePro.calculateMovingAverages(tecDataList)
 
-# Generate Intersection coordinates
-PrePro.calculateIntersects(tecDataList)
+    # Generate difference between moving average and Tec Diff
+    PrePro.calculateDelta(tecDataList)
 
-# Generate Intersection Lat and Lons
-PrePro.calculateIntersecsLatLon(tecDataList)
+    # Generate Intersection coordinates
+    PrePro.calculateIntersects(tecDataList)
 
-#####################################################################
-################### Store Final Processed Data ######################
-#####################################################################
+    # Generate Intersection Lat and Lons
+    PrePro.calculateIntersecsLatLon(tecDataList)
 
-# Function to run lat lon algorithm and store final data values
-processedTecDataList = Pros.importProcessedDataToClassList(tecDataList)
+    #####################################################################
+    ################### Store Final Processed Data ######################
+    #####################################################################
 
-# Generate and Store Pixel Array as CSV for each hour of the day
-Pros.saveProcessedTecDeltaPixelPerHr(processedTecDataList)
+    # Function to run lat lon algorithm and store final data values
+    processedTecDataList = Pros.importProcessedDataToClassList(tecDataList)
 
-# Display Proccessed TEC Delta on world map per hour
-Pros.displayProcessedTecDeltaWorldMapPerHr(processedTecDataList)
+    # Generate and Store Pixel Array as CSV for each hour of the day
+    Pros.saveProcessedTecDeltaPixelPerHr(processedTecDataList)
+
+print("Progress of CSV Export: 100% - All CSV files with Pixel Data produced for 2020!")
+
+## Display Proccessed TEC Delta on world map per hour
+#Pros.displayProcessedTecDeltaWorldMapPerHr(processedTecDataList)
 
 #####################################################################
 ################### Display Pre Processing Data #####################
 #####################################################################
 
-# Display Smooth and non-smooth vs Lat (smooth = 8 hour 30 for LEO 1 PRN 1, noisy = )
-PrePro.displayTecDiffVsLatSpecific(tecDataList)
+## Display Smooth and non-smooth vs Lat (smooth = 8 hour 30 for LEO 1 PRN 1, noisy = )
+#PrePro.displayTecDiffVsLatSpecific(tecDataList)
 
-# Display each measurement distances to validate geometry and intersect point seclection
-PrePro.displayIntersectDist(tecDataList)
+## Display each measurement distances to validate geometry and intersect point seclection
+#PrePro.displayIntersectDist(tecDataList)
 
-# Display intersect sor specific satellites vs lat
-while(1):
-    PrePro.displayIntersectsVsLatSpecific(tecDataList)
+## Display intersect sor specific satellites vs lat
+#while(1):
+#    PrePro.displayIntersectsVsLatSpecific(tecDataList)
 
-# Display Delta at P2 on world map per hour
-PrePro.displayTecDeltaWorldMapAtP2PerHr(tecDataList)
+## Display Delta at P2 on world map per hour
+#PrePro.displayTecDeltaWorldMapAtP2PerHr(tecDataList)
 
-# Display Intersect Lat vs Delta
-PrePro.displayIntersectsVsLat(tecDataList)
+## Display Intersect Lat vs Delta
+#PrePro.displayIntersectsVsLat(tecDataList)
 
-# Display Delta at P2 on world map
-PrePro.displayTecDeltaWorldMapAtP2(tecDataList)
+## Display Delta at P2 on world map
+#PrePro.displayTecDeltaWorldMapAtP2(tecDataList)
 
-# Display the Delta vs time
-PrePro.displayDeltaVsUtc(tecDataList)
+## Display the Delta vs time
+#PrePro.displayDeltaVsUtc(tecDataList)
 
-# Display the Delta vs time specific
-PrePro.displayDeltaVsUtcSpecific(tecDataList)
+## Display the Delta vs time specific
+#PrePro.displayDeltaVsUtcSpecific(tecDataList)
 
-# Display the Delta on world map
-PrePro.displayTecDeltaWorldMap(tecDataList)
+## Display the Delta on world map
+#PrePro.displayTecDeltaWorldMap(tecDataList)
 
 #####################################################################
 ############# Display Raw Data Before Pre Processing ################
 #####################################################################
 
-# Display the TEC Diff vs elevation
-Display.displayTecDiffVsElevation(tecDataList)
+## Display the TEC Diff vs elevation
+#Display.displayTecDiffVsElevation(tecDataList)
 
-# Display the TEC vs elevation
-Display.displayTecVsElevation(tecDataList)
+## Display the TEC vs elevation
+#Display.displayTecVsElevation(tecDataList)
 
-# Display TEC Diff vs UTC time
-Display.displayTecDiffVsUtc(tecDataList)
+## Display TEC Diff vs UTC time
+#Display.displayTecDiffVsUtc(tecDataList)
 
-# Display TEC Diff vs UTC time specific
-Display.displayTecDiffVsUtcSpecific(tecDataList)
+## Display TEC Diff vs UTC time specific
+#Display.displayTecDiffVsUtcSpecific(tecDataList)
 
-# Display TEC Diff measurements on world map
-Display.displayTecDiffWorldMap(tecDataList)
+## Display TEC Diff measurements on world map
+#Display.displayTecDiffWorldMap(tecDataList)
 
-# Display TEC measurements on world map
-Display.displayTecWorldMap(tecDataList)
+## Display TEC measurements on world map
+#Display.displayTecWorldMap(tecDataList)
 
-# Display TEC vs UTC time
-Display.displayTecVsUtc(tecDataList)
+## Display TEC vs UTC time
+#Display.displayTecVsUtc(tecDataList)
 
-# Display TEC vs UTC time for specific PRN and LEO ID
-Display.displayTecVsUtcSpecific(tecDataList)
+## Display TEC vs UTC time for specific PRN and LEO ID
+#Display.displayTecVsUtcSpecific(tecDataList)
