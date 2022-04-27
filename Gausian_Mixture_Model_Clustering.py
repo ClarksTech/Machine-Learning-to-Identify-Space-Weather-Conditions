84916b461aafae75b6620915ba39439738809ea9
ClarksTech <c.rutter50@gmail.com>
2022-04-27 12:36:32 +0100

    removed unused includes

--- Gausian_Mixture_Model_Clustering.py
+++ Gausian_Mixture_Model_Clustering.py
@@ -1,7 +1,4 @@
 # include required files
-from cgi import test
-import csv
-from pickle import FALSE
 import pandas as pd                             # pandas to recover pandas data frames from csv files
 import numpy as np                              # numpy for data manipulation to ML  model
 from pathlib import Path                        # to iterate over all files in folder
